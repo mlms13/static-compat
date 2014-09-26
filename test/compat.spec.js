2e7763532b703c20105a80e21f6beb4ac52472b8
Michael Martin-Smucker <mlmartin13@gmail.com>
2014-09-26 15:39:01 -0600

    Fix method name in test

--- test/compat.spec.js
+++ test/compat.spec.js
@@ -2,7 +2,7 @@ var should = require('chai').should(),
     compat = require('..');
 
 describe('compat', function () {
-  describe('#parse', function () {
+  describe('#check', function () {
     it('should return a non-empty error array for Array.prototype.filter', function () {
       compat.check('[].filter()').errors.length.should.not.be.empty;
     });
